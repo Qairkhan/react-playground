738a953a982083f881e7296af4b10e03259fc01b
Qairkhan <shyngys94@gmail.com>
2020-03-31 20:15:22 +0300

    start marterialUI

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './App.css'
+
+const App = () => {
+    return (
+        <div>   
+            <h2>Test test</h2>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'typeface-roboto';
+import App from './App'
 
-ReactDOM.render(<h1>Helloworld React!</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, <h1>Helloworld React!</h1>, document.getElementById('root'));
