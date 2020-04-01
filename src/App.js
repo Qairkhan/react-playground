89ece5cbc5ad28ab90a43a496e7748c9d1610b9d
qairkhan <shyngys94@gmail.com>
2020-04-01 17:57:30 +0300

    added console log text field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { TextField } from '@material-ui/core';
 
 import './App.css'
 
-const App = () => {
-    return (
-        <div>   
-            <h2>Test test</h2>
-        </div>
-    )
-}
+
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      console.log(this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            
+            <TextField type="text" label ="ConsoleLog" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
+ export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import 'typeface-roboto';
 import App from './App'
 
-ReactDOM.render(<App />, <h1>Helloworld React!</h1>, document.getElementById('root'));
+ReactDOM.render(<App /> , document.getElementById('root'));
